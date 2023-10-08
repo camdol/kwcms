d576290f0852e3208b7945d6b0e0c034d2a5fa72
camdol <116324476+camdol@users.noreply.github.com>
2023-10-08 23:31:44 +0900

    update 1008

--- etc/designs/kia-global/components/owners-new/IMG04/clientlib/js/img04.js
+++ etc/designs/kia-global/components/owners-new/IMG04/clientlib/js/img04.js
@@ -5,12 +5,13 @@ let cmpntIMG04Swiper;
 let swiperImg04Options;
 (function () {
     swiperImg04Options = {
-      loop:false,
-      slidesPerView: 1.5,
-      slidesPerGroup: 1,
+      loop:true,
+      slidesPerView: 'auto',
       spaceBetween: 24,
       initialSlide:0,
       draggable: true,
+      observer : true,
+      observeParents : true,
       pagination: {
           el: ".cmpnt-img04__swiper .swiper-pagination",
           clickable: true,
@@ -19,29 +20,39 @@ let swiperImg04Options;
         nextEl: '.cmpnt-img04__swiper .swiper-button-next',
         prevEl: '.cmpnt-img04__swiper .swiper-button-prev',
       },
-      observer: true,
-      observeParents: true, 
-      watchOverflow : true,
       breakpoints: {
         1025: {
-            //loop:true,
-            slidesPerView: 'auto',
-            slidesPerGroup: 3,
-            spaceBetween: 40,
-            //loopFillGroupWithBlank :true,
-            //slideOffsetAfter: 400,
+            slidesPerView: 2,
+            centeredSlides: true,
+            spaceBetween: 72,
         },
-        768: {
-          loop:false,
-          slidesPerView: 2.5,
-          slidesPerGroup: 2,
-          spaceBetween: 24,
-          //loopFillGroupWithBlank :true,
-          //slideOffsetAfter: 400,
+        767: {
+          slidesPerView: 2,
+          centeredSlides: true,
+            spaceBetween: 28,
+        },
       },
+      on: {
+        init: function() {
+          const pagination = this.pagination.el;
+          const naviNext = this.navigation.nextEl;
+          const naviPrev = this.navigation.prevEl;
+          const activeImgH = this.slides[this.activeIndex].querySelector('img').offsetHeight;
+          const activeImgW = this.slides[this.activeIndex].querySelector('img').offsetWidth / 2;
+          const pagenationGap = window.innerWidth > 1024 ? 35 : 28;
+          const naviGap = window.innerWidth > 1024 ? 180 : 68;
+          const naviTop = window.innerWidth > 1024 ? (activeImgH / 2) - 20 + 'px' : (activeImgH / 2) + 'px';
+          pagination.style.top = activeImgH + pagenationGap + 'px';
+          naviNext.style.top = naviTop;
+          naviNext.style.left = `calc(50% + ${activeImgW}px + ${naviGap}px)`;
+          naviPrev.style.top = naviTop;
+          naviPrev.style.left = `calc(50% - ${activeImgW}px - 260px)`;
+          console.log(activeImgH);
+        },
+        
       },
     };
-  
+   
 })();
 
 function initIMG04Swiper() {
