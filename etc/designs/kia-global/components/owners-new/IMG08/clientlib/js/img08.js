7d6a6d0a7cfbbb5e7634a0a1e1ed5bcb90cd6126
최동희 <chorogi@ipartners.co.kr>
2023-10-12 20:33:46 +0900

    update 1012-3

--- etc/designs/kia-global/components/owners-new/IMG08/clientlib/js/img08.js
+++ etc/designs/kia-global/components/owners-new/IMG08/clientlib/js/img08.js
@@ -8,17 +8,20 @@ let swiperImg08Options;
 })();
 
 function initIMG08Swiper() {
-  const targetSwipers = document.querySelectorAll('.cmpnt-img08');
+  const targetSwipers = document.querySelectorAll('.cmpnt-img08__wrap');
   const winW = window.innerWidth;
   const cmpntIMG08Swipers = [];
 
   Array.prototype.forEach.call(targetSwipers, function(el, i){
     const slides = el.querySelectorAll('.cmpnt-img08__item');
     const swiperBtn = el.querySelector('.swiper-button');
-    const targetSwiper = el.querySelector('.cmpnt-img08__swiper');
+    const targetSlide = el.querySelector('.cmpnt-img08__swiper');
     const naviNext = el.querySelector('.swiper-button-next');
     const naviPrev = el.querySelector('.swiper-button-prev');
     const pagination = el.querySelector('.swiper-pagination');
+
+    slides.length === 3 && targetSlide.classList.add('col3');
+    slides.length === 2 && targetSlide.classList.add('col2');
 
     swiperImg08Options = {
       loop:false,
@@ -34,10 +37,12 @@ function initIMG08Swiper() {
         nextEl: naviNext,
         prevEl: naviPrev,
       },
+      observer: true,
+      observeParents: true, 
       breakpoints: {
         1025: {
-          slidesPerView: 'auto',
-          spaceBetween: 40,
+            slidesPerView: 'auto',
+            spaceBetween: 40,
         },
         768: {
           slidesPerView: 'auto',
@@ -45,39 +50,18 @@ function initIMG08Swiper() {
         },
       },
     };
+    
+    if(slides.length > 3) {
+      let groupNum;
+      if(window.innerWidth >= 768) {
+        groupNum = window.innerWidth > 1024 ? 3 : 2;
+      } else {
+        groupNum = 1;
+      }
 
-    if(winW > 1024) {  //PC
-      if(slides.length == 3) {
-        targetSwiper.classList.add('col3');
-      }
-      if(slides.length == 2) {
-        targetSwiper.classList.add('col2');
-      }
-      if(slides.length > 3) {
-      
-        var targetSlides = el.querySelectorAll(".cmpnt-img08__item");
-        console.log(targetSlides);
-        // for (var i = 0; i < slides.length; i += 3) {
-        //   targetSlides.slice(i, i + 3).wrapAll('<div class="swiper-slide"></div>');
-        // }
-        cmpntIMG08Swipers[i] = new Swiper(targetSlide, swiperImg08Options);
-        swiperBtn.style.display = 'block';
-      }
-    } else if(winW > 767 && winW < 1025) {
-      if(slides.length == 2) {
-        targetSwiper.classList.add('col2');
-      }
-      if(slides.length > 2) {
-        //slides.chunk(2).wrap('<div class="swiper-slide"></div>');      
-        cmpntIMG08Swipers[i] = new Swiper(targetSlide, swiperImg08Options);
-        swiperBtn.style.display = 'block';
-      }
-    } else if(winW < 768) {
-      //slides.chunk(1).wrap('<div class="swiper-slide"></div>');      
+      $(".cmpnt-img08__swiper .cmpnt-img08__item").chunk(groupNum).wrap('<div class="swiper-slide"></div>');      
       cmpntIMG08Swipers[i] = new Swiper(targetSlide, swiperImg08Options);
       swiperBtn.style.display = 'block';
     }
-   
-      
   });
 }
