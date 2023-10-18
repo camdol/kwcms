8a56df3099125f2f5cf6359c89df6e4a12ee584e
최동희 <chorogi@ipartners.co.kr>
2023-10-18 20:55:49 +0900

    update 1018

--- etc/designs/kia-global/components/owners-new/LST04/clientlib/js/lst04.js
+++ etc/designs/kia-global/components/owners-new/LST04/clientlib/js/lst04.js
@@ -1,6 +1,7 @@
 'use strict';
 
 //#Component : 
+let cmpntLst04Swiper;
 (function () {
     window.addEventListener("DOMContentLoaded", categoryToggle);
 
@@ -19,4 +20,76 @@
             }
         });
     }
+
+    let paginationCurr = document.querySelector('.slide-pagination--current');
+    const paginationTotal = document.querySelector('.slide-pagination--total');
+    const options = {
+        loop:false,
+        slidesPerView: 1,
+        spaceBetween: 20,
+        initialSlide:0,
+        draggable: true,
+        navigation: {
+            nextEl: '.cmpnt-lst04__list .swiper-button-next',
+            prevEl: '.cmpnt-lst04__list .swiper-button-prev',
+        },
+        on: {
+            init: function () {
+                const totalSlides = this.slides.length;
+                paginationCurr.innerText = Number(this.realIndex + 1);
+                paginationTotal.innerText = totalSlides;
+            },
+            slideChange: function () {
+                const totalSlides = this.slides.length;
+                paginationCurr.innerText = Number(this.realIndex + 1);
+                if(this.realIndex === (totalSlides - 1)) {
+                    document.querySelector('.cmpnt-pagination--next').classList.add('is-disabled');
+                }
+                if(this.realIndex < (totalSlides - 1)) {
+                    document.querySelector('.cmpnt-pagination--next').classList.remove('is-disabled');
+                }
+                if(this.realIndex === 0) {
+                    document.querySelector('.cmpnt-pagination--prev').classList.add('is-disabled');
+                }
+                if(this.realIndex === 1) {
+                    document.querySelector('.cmpnt-pagination--prev').classList.remove('is-disabled');
+                }
+            }
+        },
+    };
+
+    window.addEventListener("DOMContentLoaded", () => {
+        if(window.innerWidth < 768 ) {
+            cmpntLst04Swiper = new Swiper(".cmpnt-lst04__list", options);
+        }
+    });
+
+    const pagination = document.querySelector('.cmpnt-lst04__slide-pagination');
+    let idx = paginationCurr.innerText - 1;
+    pagination.addEventListener('click', function(e) {
+        const target = e.target;
+        const totalNumber = paginationTotal.innerText - 1;
+        idx = paginationCurr.innerText - 1;
+        console.log(target);
+        if(idx < totalNumber && target.classList.contains('cmpnt-pagination--next')) {
+            idx = parseInt(idx) + 1;
+            if(idx === totalNumber) {
+                target.classList.add('is-disabled');
+            }
+            if(idx === 1) {
+                document.querySelector('.cmpnt-pagination--prev').classList.remove('is-disabled');
+            }
+        } else if(idx > -1 && target.classList.contains('cmpnt-pagination--prev')) {
+            idx = idx - 1;
+            if(idx === (totalNumber - 1)) {
+                document.querySelector('.cmpnt-pagination--next').classList.remove('is-disabled');
+            }
+            if(idx === 1) {
+                target.classList.remove('is-disabled');
+            }
+        } 
+        cmpntLst04Swiper.slideTo(idx);
+    });
+
+    
 })();
--- etc/designs/kia-global/components/owners-new/LST05/clientlib/js/lst05.js
+++ etc/designs/kia-global/components/owners-new/LST05/clientlib/js/lst05.js
@@ -1,7 +1,21 @@
 'use strict';
 
-//#Component : 
+//#Component : lst05 : 검수용
 (function () {
-   
+    window.addEventListener("DOMContentLoaded", setSelect);
+
+    // selectbox select value
+    function setSelect() {
+        const selectEls = document.querySelectorAll('.cmpnt-src-select__item');
+        Array.prototype.forEach.call(selectEls, function(el){
+            el.addEventListener('click', e =>  {
+                const target = e.target;
+                target.closest('.cmpnt-src-select').setAttribute('value', target.innerText.trim());
+                target.closest('ul').querySelector('.is-select') && target.closest('ul').querySelector('.is-select').classList.remove('is-select');
+                target.classList.add('is-select');
+                target.closest('.cmpnt-src-select').classList.remove('is-show');
+            });
+        });
+    }
 
 })();
